00d20bf50ccdb6299bf943a662742267d6f6594d
Anton31 <antohaspy070@gmail.com>
2020-08-01 19:31:01 +0300

    Убрал классы находящиеся в разработке

--- src/main/java/ru/careportal/core/aspects/userDataAOP.java
+++
@@ -1,9 +0,0 @@
-package ru.careportal.core.aspects;
-
-import org.aspectj.lang.annotation.Aspect;
-import org.springframework.context.annotation.Configuration;
-
-@Aspect
-@Configuration
-public class userDataAOP {
-}

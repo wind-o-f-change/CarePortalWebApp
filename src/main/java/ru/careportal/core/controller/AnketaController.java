73e0aa6a57dfae36bcc61c9f79c86251324462b0
Артём Галай <artem.galay.14@gmail.com>
2020-08-07 14:08:38 +0300

    Конструктор анкет v0.12 @PreAuthorize("isAuthenticated()")

--- src/main/java/ru/careportal/core/controller/AnketaController.java
+++ src/main/java/ru/careportal/core/controller/AnketaController.java
@@ -1,6 +1,7 @@
 package ru.careportal.core.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,6 +19,7 @@ import java.security.Principal;
 import java.util.List;
 
 @Controller
+@PreAuthorize("isAuthenticated()")
 public class AnketaController {
     private PassedAnketaService passedAnketaService;
     private QuestionService questionService;

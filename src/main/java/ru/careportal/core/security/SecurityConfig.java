1cc096d73b6c283df1618a95f6dc36464a6c6aaf
k.ushakova.stc%40innopolis.university <k.ushakova.stc@innopolis.university>
2020-08-07 19:23:21 +0500

    Убраны лишние импорты

--- src/main/java/ru/careportal/core/security/SecurityConfig.java
+++ src/main/java/ru/careportal/core/security/SecurityConfig.java
@@ -3,7 +3,6 @@ package ru.careportal.core.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -12,7 +11,6 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
-import ru.careportal.core.db.model.Role;
 import ru.careportal.core.service.UserDetailsServiceImpl;
 
 @Configuration

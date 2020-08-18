442f1cdc929ed843545ea9f1a4cde438f2fbe9ca
Артём Галай <artem.galay.14@gmail.com>
2020-08-18 19:22:43 +0300

    переадресация в ЛК авторизованного юзера со странички логина

--- src/main/java/ru/careportal/core/controller/SecurityController.java
+++ src/main/java/ru/careportal/core/controller/SecurityController.java
@@ -1,16 +1,24 @@
 package ru.careportal.core.controller;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import ru.careportal.core.db.model.Role;
 import ru.careportal.core.db.model.User;
 import ru.careportal.core.security.RegistrationForm;
+import ru.careportal.core.security.UserStartPageDeterminant;
 import ru.careportal.core.service.UserService;
 
+import java.security.Principal;
+import java.util.Map;
 import java.util.Optional;
 @Slf4j
 @Controller
@@ -52,9 +60,26 @@ public class SecurityController {
 
     @GetMapping({ "/", "/login" })
     public String loginFailure(@RequestParam(value = "error", required = false) String error, Model model) {
+        if (isAuthenticated()) {
+            Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+            String authorityName = authentication.getAuthorities().stream().findFirst().get().getAuthority();
+            Role role = Role.valueOf(authorityName);
+            String s = "redirect:".concat(new UserStartPageDeterminant().definePageByRole(role));
+            return s;
+        }
+
         if (error != null && error.isEmpty()) {
             model.addAttribute("error", "Аккаунт не подтвержден администратором или использован неверный e-mail/пароль");
         }
         return "login";
+    }
+
+    private boolean isAuthenticated() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication == null || AnonymousAuthenticationToken.class.
+                isAssignableFrom(authentication.getClass())) {
+            return false;
+        }
+        return authentication.isAuthenticated();
     }
 }
